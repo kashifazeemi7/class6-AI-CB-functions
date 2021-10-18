9d69ca6f7c12607271abb0d3e8fe587af35e7505
Kashif Azeemi <61817392+kashifazeemi7@users.noreply.github.com>
2021-10-18 23:04:38 +0500

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+function values() {
+var ele11 = document.getElementById('elem-11').value
+var ele12 = document.getElementById('elem-12').value
+var ele13 = document.getElementById('elem-13').value
+var ele14 = document.getElementById('elem-14').value
+var ele15 = document.getElementById('elem-15').value
+var ele16 = document.getElementById('elem-16').value
+
+
+var ele21 = document.getElementById('elem-21').value
+var ele22 = document.getElementById('elem-22').value
+var ele23 = document.getElementById('elem-23').value
+var ele24 = document.getElementById('elem-24').value
+var ele25 = document.getElementById('elem-25').value
+var ele26 = document.getElementById('elem-26').value
+}
+
+    function sum() {
+        
+    }
+
+    
